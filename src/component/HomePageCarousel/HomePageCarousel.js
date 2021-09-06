0a2943cd93ad250ca3a657777f275a4c0b17f2ef
Shomikghosh <59383763+Shomikghosh@users.noreply.github.com>
2021-09-06 23:43:09 +0530

    Done

--- src/component/HomePageCarousel/HomePageCarousel.js
+++ src/component/HomePageCarousel/HomePageCarousel.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import g2 from '../../assests/homepagecarasoul/g2.jpeg';
-import g3 from '../../assests/homepagecarasoul/g3.jpeg';
-import g4 from '../../assests/homepagecarasoul/g4.jpeg';
-import g5 from '../../assests/homepagecarasoul/g5.jpeg';
-import g6 from '../../assests/homepagecarasoul/g6.jpeg';
+import g2 from '../../assests/teamcarasoul/f1.png';
+import g3 from '../../assests/GALLERY/g6.jpeg';
+import g4 from '../../assests/GALLERY/e7.jpg';
+import g5 from '../../assests/GALLERY/g8.jpg';
+// import g6 from '../../assests/homepagecarasoul/g6.jpeg';
 // import g8 from '../../assests/homepagecarasoul/g8.jpg';
-import g9 from '../../assests/homepagecarasoul/g9.jpg';
+// import g9 from '../../assests/homepagecarasoul/g9.jpg';
 import styled from 'styled-components'
 import BangaloreBg from '../../assests/images/bangalore_bg.jpg'
 function HomePageCarousel() {
@@ -19,31 +19,36 @@ function HomePageCarousel() {
                     // autoPlay={true}	
                     // interval={5000}
                     >
-                    <div>
+                    <Sty>
                         <img src={g2} alt="ard"/>
-                    </div>
-                    <div>
+                    </Sty>
+                    <Sty>
                         <img src={g3} alt="orn"/>
-                    </div>
-                    <div>
+                    </Sty>
+                    <Sty>
                         <img src={g4} alt="snap"/>
-                    </div>
-                    <div>
+                    </Sty>
+                    <Sty>
                         <img src={g5} alt="snap"/>
-                    </div>
-                    <div>
+                    </Sty>
+                    {/* <div>
                         <img src={g6} alt="snap"/>
-                    </div>
+                    </div> */}
                     {/* <div>
                         <img src={g8} alt="snap"/>
                     </div> */}
-                    <div>
+                    {/* <div>
                         <img src={g9} alt="snap"/>
-                    </div>
+                    </div> */}
             </Carousel></MainContainer>
         </>
     )
 }
+const Sty = styled.div`
+  >img{
+        height:700px;
+    }
+`;
 
 const MainContainer = styled.div`
     width: 80%;
@@ -53,6 +58,7 @@ const MainContainer = styled.div`
     @media only screen and (max-width: 780px) {
     width:100%;
     }
+  
 `;
 
 export default HomePageCarousel
